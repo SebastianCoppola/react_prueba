9f1a1c50a0b8cd3b7d47c7da90e71b67b4c5724b
SebastianCoppola <seba.coppola@hotmail.com>
2022-05-16 09:23:02 -0300

    update 16mayo

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,29 +1,33 @@
 import React, {useEffect, useRef} from 'react';
 
 const SearchBar = () => {
-    
+
     const focusInputRef = useRef();
+
     useEffect(()=>{
         focusInputRef.current.focus();
     },[])
-    const handleClick = () => {
-        console.log("Buscaste '" + focusInputRef.current.value + "'.");    
-    }
+
+    // const handleClick = () => {
+    //     console.log("Buscaste '" + focusInputRef.current.value + "'.");
+    // }
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        focusInputRef.current.value = ""; 
+        console.log("Buscaste '" + focusInputRef.current.value + "'.");
+        focusInputRef.current.value = "";
     }
 
     return (
     <div className="search-bar">
         <form className="form" onSubmit={handleSubmit}>
-            <input 
-                type="search" 
-                id="search" 
+            <input
+                type="search"
+                id="search"
                 placeholder="buscar"
                 ref={focusInputRef}
             />
-            <button onClick={handleClick}>🔍</button>
+            <button /*onClick={handleClick}*/>🔍</button>
         </form>
     </div>
     )
